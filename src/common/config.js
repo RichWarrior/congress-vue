0ed5e5c8d5288276c6a204e467e1934e24ccff7d
Faruk Şahin <faruk_thecno@hotmail.com>
2019-10-22 21:02:54 +0300

    Server Info

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,7 @@
 //Dev
 //export const API_URL = "http://localhost:50548/api";
 //Prod
-export const API_URL = "http://212.154.81.35:8080/api";
+//export const API_URL = "http://212.154.81.35:8080/api";
 //export const API_URL = 'http://localhost:5000/api'
+export const API_URL ='http://api.congretic.com/api';
 export default API_URL;
