32e5f37ad74c9b4d4eed0be1fe316b13b21ebd3d
unknown <faruk_thecno@hotmail.com>
2019-12-05 19:53:25 +0300

    CONFIG FILE CHANGED

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 //Dev
-//export const API_URL = "http://localhost:50548/api";
+// export const API_URL = "http://localhost:50548/api";
 //Prod
 //export const API_URL = "http://212.154.81.35:8080/api";
 //export const API_URL = 'http://localhost:5000/api'
